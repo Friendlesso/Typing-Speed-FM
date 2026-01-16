8c21583838b1eb4fb85146ea1e2f2f2f373ef444
Mihailo <mihailoless@gmail.com>
2026-01-16 22:49:21 +0100

    Fix DropdownMenu styles for better responsiveness

--- src/components/TypingHUD/components/DropdownButton/DropdownMenu.tsx
+++ src/components/TypingHUD/components/DropdownButton/DropdownMenu.tsx
@@ -38,7 +38,7 @@ export function DropdownMenu<T>({
         flex flex-col gap-2 py-2.5
         transition-[max-height, opacity] duration-300 ease-in-out
         ${langStyle
-          ? 'overflow-none w-full right-0 '
+          ? 'overflow-none w-fit right-0 '
           : 'overflow-hidden sm:w-full w-fit left-0'
         }
         ${isOpen
