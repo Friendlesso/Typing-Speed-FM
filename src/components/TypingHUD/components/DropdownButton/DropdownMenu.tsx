a294bb42c08eeef24e25118ba2c984dda44dacdb
Mihailo <mihailoless@gmail.com>
2026-01-16 22:37:46 +0100

    Fixed button styling issues

--- src/components/TypingHUD/components/DropdownButton/DropdownMenu.tsx
+++ src/components/TypingHUD/components/DropdownButton/DropdownMenu.tsx
@@ -39,8 +39,8 @@ export function DropdownMenu<T>({
         flex flex-col gap-2 py-2.5
         transition-[max-height, opacity] duration-300 ease-in-out
         ${langStyle
-          ? 'overflow-none sm:w-fit w-full right-0 '
-          : 'overflow-hidden w-full left-0'
+          ? 'overflow-none sm:w-full w-fit right-0 '
+          : 'overflow-hidden sm:w-full w-fit left-0'
         }
         ${isOpen
           ? 'max-h-90 opacity-100  pointer-events-auto'
